155ac48a1fcad0555954b1bddb57b2674990e82c
Rich Zhu <rich@Richs-MBP.wv.cc.cmu.edu>
2018-10-14 14:50:02 -0400

    Added feature of writing results to a file and generating a scatter plot

--- win_ratio.py
+++ win_ratio.py
@@ -12,6 +12,7 @@ import copy
 import json
 from datetime import datetime
 import os
+import matplotlib.pyplot as plt
 
 
 start_year = 2010
@@ -44,9 +45,6 @@ def add_games_each_month(game_dates, play_data):
         play_date = play_data[i][0]
         year, month = play_date.split('-')
         year, month = int(year), int(month)
-        # print(game_dates)
-        # print("year is " + str(year))
-        # print("month is " + str(month))
         if (date_out_of_range(year, month)): continue
         game_dates[year][month] += 1
 
@@ -64,14 +62,12 @@ def find_real_end_time(game_dates):
         for month in range(months, 0, -1):
             if game_dates[year][month] != 0:
                 return (year, month)
-    # print("Never played the game!")
-
 
 # inactive period: number of months consecutively where players play less than 5 games in each month 
 # find the start month of the inactive period, its duration, and the win ratio 
 # find the 10 games before the start of the inactive period
+game_bar = 5 # threshold for the minimum number of games each month to be considered active
 def find_inactive_period(start_year, start_month, end_year, end_month, game_dates, result_dict, play_data):
-    game_bar = 2 # threshold for the minimum number of games each month to be considered active
     first_inactive = False
     inactive_start = False
     inactive_period = 0
@@ -84,46 +80,34 @@ def find_inactive_period(start_year, start_month, end_year, end_month, game_date
         if (year == end_year): over_month = end_month
         for month in range(begin_month, over_month + 1):
             num_games = game_dates[year][month]
-            # print("Year is " + str(year))
-            # print("Month is " + str(month))
-            # print("Number of games is " + str(num_games))
             if (num_games < game_bar): # this month is inactive
-                if (inactive_start == True):
+                if (inactive_start == True): # inactive period started
                     inactive_period += 1
-                    # print("Continue adding inactive period")
                     # if it is the last month and year
                     if (year == end_year and month == end_month):
-                        # print("Adding the inactive period!!!")
                         update_result(result_dict, inactive_year, inactive_month, inactive_period, start_year, start_month, play_data)
                 else: # inactive period has not yet started
                     if (first_inactive == False): 
                         first_inactive = True
                         inactive_month = month
                         inactive_year = year
-                        # print("First inactive period")
-                    else:
+                    else: # Initiate inactive period
                         inactive_start = True
                         inactive_period += 2
-                        # print("Start inactive period")
                         if (year == end_year and month == end_month):
-                            # print("Adding the inactive period!!!")
                             update_result(result_dict, inactive_year, inactive_month, inactive_period, start_year, start_month, play_data)
             else: # this month is active
                 if (inactive_start == True or first_inactive == True): # terminate this inactive period
                     if (inactive_start == True):
-                        # print("Adding the inactive period!!!")
                         update_result(result_dict, inactive_year, inactive_month, 
                                       inactive_period, start_year, start_month, play_data)
                     first_inactive = False
                     inactive_start = False
-                    # print("Ending the inactive period")
                     inactive_period = 0
-            # print()
 
 
 def update_result(result_dict, inactive_year, inactive_month, inactive_period, start_year, start_month, play_data):
     win_ratio = find_win_ratio(play_data, inactive_year, inactive_month, start_year, start_month)
-    # print("win ratio is ", win_ratio)
     if win_ratio == None: return # No data available
     if inactive_period in result_dict:
         data_list = result_dict[inactive_period]
@@ -131,10 +115,9 @@ def update_result(result_dict, inactive_year, inactive_month, inactive_period, s
     else:
         result_dict[inactive_period] = [win_ratio]
 
-# bug!!!!
+backtrace = 5 # backtrace 5 games
 def find_win_ratio(play_data, inactive_year, inactive_month, start_year, start_month):
     wins = 0
-    backtrace = 5 # backtrace 5 games
     num_data = 0
     for year in range(inactive_year, start_year - 1, -1):
         month1 = 12
@@ -153,7 +136,6 @@ def find_win_ratio(play_data, inactive_year, inactive_month, start_year, start_m
                             wins += 1     
     if num_data != 0: return float(wins) / num_data
     else: return None   
-
 
 
 def process_play_data(play_data):
@@ -178,6 +160,23 @@ def average_result(result):
         data_list = result[key]
         result[key] = find_average(data_list)
 
+def split_data(result):
+    inactive_period = []
+    win_ratio = []
+    for period in result:
+        inactive_period.append(period)
+        win_ratio.append(result[period])
+    return (inactive_period, win_ratio)
+
+
+def write_result(fileName, inactive_and_win, backtrace, game_bar):
+    fh = open(fileName, "w")
+    fh.write("Less than " + str(game_bar) + " number of games for 2 consecutive months is considered inactive\n")
+    fh.write("Results of " + str(backtrace) + " number of games is tracked before start of inactive period\n")
+    fh.write("Results are listed below:\n")
+    for period in inactive_and_win:
+        fh.write(str(period) + ": " + str(inactive_and_win[period]) + "\n")
+    fh.close()
 
 # Time interval of the data
 # %Y-%m-%d %H:%M:%S  data formatting
@@ -187,11 +186,11 @@ def average_result(result):
 # a dictionary where the key is the length of the inactive period and the value
 # is the array of winning ratio values
 inactive_and_win = dict()
-path = '../Player_Analysis/split_player_11/'
+path = '../Player_Analysis/small_data/'
 # browse through all files in the directory
 for root, dirs, files in os.walk(path):
     for name in files:
-        print(name)
+        # print(name)
         with open(path + name) as json_data:
             play_data = json.load(json_data)
             for i in range(0, len(play_data)):
@@ -212,13 +211,13 @@ for root, dirs, files in os.walk(path):
                 play_data = process_play_data(play_data)
                 # find inactive period, calculate the win ratio, and put it into result
                 find_inactive_period(real_start_year, real_start_month, real_end_year, real_end_month, game_dates, inactive_and_win, play_data)
-            
-            # print(play_data)
-            # print(inactive_and_win)
-            # print((start_year, start_month), (end_year, end_month))
-            # print(play_data)
 average_result(inactive_and_win)
+inactive_period, win_ratio = split_data(inactive_and_win)
+output_name = str(game_bar) + "Games" + str(backtrace) + "Backtrace"
+write_result(output_name, inactive_and_win, backtrace, game_bar) 
 print(inactive_and_win)
+plt.scatter(win_ratio, inactive_period)
+plt.show()
 
 
 
